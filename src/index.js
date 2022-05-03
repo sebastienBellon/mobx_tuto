4726e9ed229eaa00f25c56434ae6bfd3077944a3
Sébastien bellon <sebastien.bellon@nagra.com>
2022-05-03 11:28:13 +0200

    add secondary whishlist item

--- src/index.js
+++ src/index.js
@@ -14,12 +14,14 @@ const whishList = WishList.create({
       image:
         "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     },
+    {
+      name: "Chronicles of Pascal",
+      price: 20,
+      image:
+        "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    },
   ],
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App whishList={whishList} />);
-
-setInterval(() => {
-  whishList.items[0].changePrice(whishList.items[0].price + 1);
-}, 1000);
